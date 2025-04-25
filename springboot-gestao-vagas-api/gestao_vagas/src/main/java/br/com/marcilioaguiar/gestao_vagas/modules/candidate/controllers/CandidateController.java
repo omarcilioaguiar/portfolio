b488bee7429e3e1990d431d68fb2ca294577637b
Marcilio Aguiar <marcilio.paguiar@gmail.com>
2025-04-24 21:00:55 -0300

    Adicionando validações e expressões regulares nos campos

--- springboot-gestao-vagas-api/gestao_vagas/src/main/java/br/com/marcilioaguiar/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ springboot-gestao-vagas-api/gestao_vagas/src/main/java/br/com/marcilioaguiar/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.marcilioaguiar.gestao_vagas.modules.candidate.CandidateEntity;
+import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class CandidateController {
 
     @PostMapping("/")
-    public void create(@RequestBody CandidateEntity candidateEntity) {
+    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
         System.out.println("Candidato");
         System.out.println(candidateEntity.getEmail());
     }
