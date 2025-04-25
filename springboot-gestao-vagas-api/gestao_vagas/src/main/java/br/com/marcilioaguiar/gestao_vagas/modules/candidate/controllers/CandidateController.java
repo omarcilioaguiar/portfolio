58a9ab80b3da20f42d053c54fcfc9de626500d87
Marcílio Aguiar <marcilio.paguiar@gmail.com>
2025-04-25 11:25:28 -0300

    Validando usuário já existente

--- springboot-gestao-vagas-api/gestao_vagas/src/main/java/br/com/marcilioaguiar/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ springboot-gestao-vagas-api/gestao_vagas/src/main/java/br/com/marcilioaguiar/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -3,6 +3,7 @@ package br.com.marcilioaguiar.gestao_vagas.modules.candidate.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.marcilioaguiar.gestao_vagas.exceptions.UserFoundException;
 import br.com.marcilioaguiar.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.marcilioaguiar.gestao_vagas.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
@@ -20,6 +21,13 @@ public class CandidateController {
 
     @PostMapping("/")
     public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        this.candidateRepository
+        .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
+        .ifPresent((user) -> {
+            throw new UserFoundException();
+        });
+
+
         return this.candidateRepository.save(candidateEntity);
     }
 
