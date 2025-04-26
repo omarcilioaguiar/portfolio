ce61480692c8bede41ca3aaf97a1f4e8bf2a0bcd
Marcílio Aguiar <marcilio.paguiar@gmail.com>
2025-04-26 13:26:58 -0300

    Implementando UseCase JWT

--- springboot-gestao-vagas-api/gestao_vagas/src/main/java/br/com/marcilioaguiar/gestao_vagas/modules/company/repositories/CompanyRepository.java
+++ springboot-gestao-vagas-api/gestao_vagas/src/main/java/br/com/marcilioaguiar/gestao_vagas/modules/company/repositories/CompanyRepository.java
@@ -6,7 +6,10 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.marcilioaguiar.gestao_vagas.modules.company.entities.CompanyEntity;
+import java.util.List;
+
 
 public interface CompanyRepository extends JpaRepository<CompanyEntity, UUID>{
     Optional<CompanyEntity> findByUsernameOrEmail(String username, String email);
+    Optional <CompanyEntity> findByUsernamet(String username);
 }
