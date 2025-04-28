fe36ac57eebbaaa1a5a126619c7a7c47d6e9dad4
Marcílio Aguiar <marcilio.paguiar@gmail.com>
2025-04-28 16:12:30 -0300

    Hash de senha de candidato e controller de auth

--- springboot-gestao-vagas-api/gestao_vagas/src/main/java/br/com/marcilioaguiar/gestao_vagas/modules/candidate/repositories/CandidateRepository.java
+++ springboot-gestao-vagas-api/gestao_vagas/src/main/java/br/com/marcilioaguiar/gestao_vagas/modules/candidate/repositories/CandidateRepository.java
@@ -6,7 +6,10 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.marcilioaguiar.gestao_vagas.modules.candidate.entities.CandidateEntity;
+import java.util.List;
+
 
 public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID>{
     Optional<CandidateEntity> findByUsernameOrEmail(String username, String email);
+    Optional<CandidateEntity> findByUsername(String username);
 }
