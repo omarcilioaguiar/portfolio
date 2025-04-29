09b24d08b58eab26fa5baf3d7445079456992ba5
Marcílio Aguiar <marcilio.paguiar@gmail.com>
2025-04-28 22:10:34 -0300

    Correcao

--- springboot-gestao-vagas-api/gestao_vagas/src/main/java/br/com/marcilioaguiar/gestao_vagas/security/SecurityCandidateFilter.java
+++ springboot-gestao-vagas-api/gestao_vagas/src/main/java/br/com/marcilioaguiar/gestao_vagas/security/SecurityCandidateFilter.java
@@ -1,13 +1,11 @@
 package br.com.marcilioaguiar.gestao_vagas.security;
 
 import java.io.IOException;
-import java.util.Collections;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
